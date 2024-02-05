d473e053b24733a8548377c951d67fd6a0c6aae6
Yohan Cantin <yohancantin22@gmail.com>
2024-02-05 16:59:09 -0500

    Update get_next_line.h
    added ft_substr.c and removed unused functions

--- get_next_line.h
+++ get_next_line.h
@@ -23,16 +23,13 @@
 #  define BUFFER_SIZE 22
 # endif
  
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 void    ft_strcpy(char *dest, char *src);
 int     ft_strlen(char *str);
 size_t  ft_strlcat(char *dest, const char *src, size_t sizeofbuf);
 size_t	ft_strlcpy(char *dest, const char *src, size_t i);
 char	*ft_strjoin(char const *s1, char const *s2);
-char    *ft_strncpy(char *dest, char *src, unsigned int n);
 char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 char    *get_next_line(int fd);
-void	*ft_calloc(size_t nitems, size_t size);
-void	ft_bzero(void *str, size_t nb);
 
 #endif
